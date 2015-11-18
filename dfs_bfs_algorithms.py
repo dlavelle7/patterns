40eeb50d89c143f7f7d9dbefeed3a1b919878ccf
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-18 16:24:18 +0000

    Added UT for create tree.

--- dfs_bfs_algorithms.py
+++ dfs_bfs_algorithms.py
@@ -8,7 +8,7 @@ class Node(object):
         Node.count += 1
 
     def __repr__(self):
-        return "Node {0}".format(self.id)
+        return "Node: {0}".format(self.id)
 
     def add_children(self, children):
         for child in children:
@@ -18,7 +18,7 @@ class Node(object):
         child.parent = self
         self.children.add(child)
 
-def create_balanced_graph(levels=4, node=None):
+def create_balanced_tree(levels=4, node=None):
     root = None
     if not node:
         node = Node()
@@ -26,15 +26,15 @@ def create_balanced_graph(levels=4, node=None):
     if levels > 0:
         node.add_children(set([Node(), Node()]))
         for child in node.children:
-            create_balanced_graph(levels - 1, child)
+            create_balanced_tree(levels - 1, child)
     return root
 
 def depth_first_search(find_node):
-    root = create_balanced_graph()
     import ipdb; ipdb.set_trace()
+    root = create_balanced_tree()
 
 def breadth_first_search(find_node):
-    root = create_balanced_graph()
     import ipdb; ipdb.set_trace()
+    root = create_balanced_tree()
 
 # TODO: pygraphviz
--- test_dfs_bfs_algorithms.py
+++ test_dfs_bfs_algorithms.py
@@ -1,5 +1,6 @@
 import unittest
-from dfs_bfs_algorithms import depth_first_search, breadth_first_search
+from dfs_bfs_algorithms import depth_first_search, breadth_first_search, \
+        create_balanced_tree, Node
 
 class TestDfsBfsAlgorithms(unittest.TestCase):
 
@@ -8,3 +9,11 @@ class TestDfsBfsAlgorithms(unittest.TestCase):
 
     def test_breadth_first_search(self):
         breadth_first_search(1)
+
+    def test_create_balanced_tree(self):
+        root = create_balanced_tree()
+        self.assertTrue(type(root) is Node)
+        for child in root.children:
+            self.assertTrue(root.id < child.id)
+
+    # TODO: Test new important functions
