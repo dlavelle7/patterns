1316df2c5bc06b4ce378ab0240bbcfe482f450fd
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-18 11:28:11 +0000

    Update simple palindrome algorithm.

--- Palindrome.java
+++ Palindrome.java
@@ -3,8 +3,8 @@ public class Palindrome {
 
 	public static boolean isPalindrome(String word){
 		word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-		for (int i = word.length() -1, count = 0; i > count; i--, count++){
-			if (word.charAt(count) != word.charAt(i))
+		for (int i = word.length() -1, j = 0; i > j; i--, j++){
+			if (word.charAt(j) != word.charAt(i))
 				return false;
 		}
 		return true;
