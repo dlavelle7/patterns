225999bba1fcb8e6952cd6a16559f6d9a2cb9ab4
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-20 09:27:32 +0000

    Basic Fibonacci.

--- algorithms/fibonacci.py
+++ algorithms/fibonacci.py
@@ -2,11 +2,19 @@
 import sys
 
 def main():
-    true_fib = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233}
+    true_fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
     assert fibonacci(len(true_fib)) == true_fib
+    true_fib = [0, 1]
+    assert fibonacci(2) == true_fib
+    true_fib = [0]
+    assert fibonacci(1) == true_fib
 
 def fibonacci(n):
     sequence = [0, 1]
+    if n < len(sequence):
+        return sequence[:n]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
     return sequence
 
 if __name__ == "__main__":
