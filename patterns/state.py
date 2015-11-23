54c0bdf5aab80a3c038e75979a89c968dbc44980
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-23 12:22:37 +0000

    Implemented absract class and concrete classes.

--- patterns/state.py
+++ patterns/state.py
@@ -1,1 +1,28 @@
-# TODO: State design pattern
+#!/usr/bin/env python
+
+from abc import ABCMeta, abstractmethod
+
+class Base(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def foo(self):
+        pass
+
+    @abstractmethod
+    def bar(self):
+        pass
+
+class BadConcrete(Base):
+    # abstract method bar() not implemented:
+    # Class created but error when instantiating
+    def foo(self):
+        return 'bad foo'
+
+class Concrete(Base):
+    def foo(self):
+        return 'foo'
+
+    def bar(self):
+        return 'bar'
---
+++ patterns/test_state.py
@@ -0,0 +1,25 @@
+import unittest
+from state import Base, Concrete, BadConcrete
+
+
+class StateDesignPatternTest(unittest.TestCase):
+
+    def _assertRaisesCustom(self, exception, message, callable, *args, **kwargs):
+        try:
+            callable(*args, **kwargs)
+        except exception as e:
+            if (str(e) != message):
+                raise AssertionError("{0} != {1}".format(message, str(e)))
+        else:
+            raise AssertionError('No exception was raised')
+
+    def test_abstract_methods(self):
+        # non implemented abstract method
+        self.assertTrue(issubclass(BadConcrete, Base))
+        message = "Can't instantiate abstract class BadConcrete with abstract methods bar"
+        self._assertRaisesCustom(TypeError, message, BadConcrete)
+
+        # all abstract classes implemented
+        concrete = Concrete()
+        self.assertEqual('foo', concrete.foo())
+        self.assertEqual('bar', concrete.bar())
