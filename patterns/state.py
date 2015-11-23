850ebda431803c6cf185c6d0f5cd087ee13f4021
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-23 17:28:25 +0000

    Draft state pattern.

---
+++ patterns/state.py
@@ -0,0 +1,36 @@
+# State Design Pattern
+from abc import ABCMeta, abstractmethod
+
+
+class MP3PlayerContext(object):
+
+    def __init__(self,state):
+        self.state = state
+
+    def play(self):
+        self.state.press_play(self)
+
+    def set_state(self, state):
+        self.state = state
+
+    def get_state(self):
+        return self.state
+
+
+class State(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def press_play(self, context):
+        pass
+
+
+class StandbyState(State):
+    def press_play(self, context):
+        context.set_state(PlayingState())
+
+
+class PlayingState(State):
+    def press_play(self, context):
+        context.set_state(StandbyState())
