a881d6f3de0a846f09e8c121ca3a0de12dfa2170
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-24 14:19:38 +0000

    Refactored bubble sort.;

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -9,24 +9,18 @@ def main():
     assert bubble_sort(test_list) == sorted(test_list)
     test_list = [2, 4, 5, 3, 2, 1]
     assert bubble_sort(test_list) == sorted(test_list)
-    test_list = [2, 4, 5, 3, 3, 1]
-    assert bubble_sort(test_list) == sorted(test_list)
-    test_list = [8, 8, 8, 5, 3, 1, 1]
+    test_list = [2, -4, -5, 3, -3, 1]
     assert bubble_sort(test_list) == sorted(test_list)
 
-def bubble_sort(sortable_list):
-    while True:
-        swapped = False
-        for index in range(len(sortable_list) -1):
-            if sortable_list[index] > sortable_list[index + 1]:
-                swapped = True
-                swap_a = sortable_list[index + 1]
-                swap_b = sortable_list[index]
-                sortable_list[index] = swap_a
-                sortable_list[index + 1] = swap_b
-        if not swapped:
-            break
-    return sortable_list
+def bubble_sort(sequence):
+    unsorted = True
+    while unsorted:
+        unsorted = False
+        for i in range(len(sequence) -1):
+            if sequence[i] > sequence[i + 1]:
+                unsorted = True
+                sequence[i +1], sequence[i] = sequence[i], sequence[i + 1]
+    return sequence
 
 if __name__ == "__main__":
     main()
