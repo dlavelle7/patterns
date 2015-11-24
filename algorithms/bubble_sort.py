7aee75a7a72a71c1ce2b70d2f607882df3a0abee
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-24 12:50:44 +0000

    Draft bubble sort implementation assertions.

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -7,16 +7,23 @@ def main():
     assert bubble_sort(test_list) == sorted(test_list)
     test_list = []
     assert bubble_sort(test_list) == sorted(test_list)
+    test_list = [2, 4, 5, 3, 2, 1]
+    assert bubble_sort(test_list) == sorted(test_list)
+    test_list = [2, 4, 5, 3, 3, 1]
+    assert bubble_sort(test_list) == sorted(test_list)
+    test_list = [8, 8, 8, 5, 3, 1, 1]
+    assert bubble_sort(test_list) == sorted(test_list)
 
 def bubble_sort(sortable_list):
     while True:
         swapped = False
-        for index, value in enumerate(sortable_list[:-1]):
-            if value > sortable_list[index + 1]:
+        for index in range(len(sortable_list) -1):
+            if sortable_list[index] > sortable_list[index + 1]:
                 swapped = True
-                swap = sortable_list[index + 1]
-                sortable_list[index] = swap
-                sortable_list[index + 1] = value
+                swap_a = sortable_list[index + 1]
+                swap_b = sortable_list[index]
+                sortable_list[index] = swap_a
+                sortable_list[index + 1] = swap_b
         if not swapped:
             break
     return sortable_list
