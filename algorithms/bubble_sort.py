cf91ad2520edf5363bab527993c4d0302e615b3c
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-24 12:34:59 +0000

    Draft bubble sort.

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -11,7 +11,6 @@ def main():
 def bubble_sort(sortable_list):
     while True:
         swapped = False
-        import ipdb; ipdb.set_trace()
         for index, value in enumerate(sortable_list[:-1]):
             if value > sortable_list[index + 1]:
                 swapped = True
