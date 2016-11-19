52927e45ceea415ce10ffeaa8a67298ab144eb30
David Lavelle <d.lavelle7@yahoo.ie>
2016-11-19 15:27:08 +0000

    added comments

--- python/singleton.py
+++ python/singleton.py
@@ -5,6 +5,7 @@ class Singleton(object):
 
     def __new__(cls, value):
         if not Singleton.instance:
+            # Call objects __new__, as cls(value) will bring you back here
             Singleton.instance = super(Singleton, cls).__new__(cls, value)
         return Singleton.instance  # return instance of 'cls' to __init__
 
@@ -18,11 +19,13 @@ class Singleton(object):
 foo = Singleton('foo')
 print foo
 
+# Udates the same class instance, with new values
 bar = Singleton('bar')
 print bar
 
 baz = Singleton('baz')
 print baz
 
+# Test
 assert baz.value == bar.value == foo.value
 assert id(foo) == id(bar) == id(baz)
