f49ba793cf8376e66ef46403e5ffd849a4fc77f4
David Lavelle <d.lavelle7@yahoo.ie>
2016-11-20 13:30:07 +0000

    added generator expression.

--- python/generator.py
+++ python/generator.py
@@ -2,16 +2,18 @@
 
 
 class FirstN(object):
-    """Generator pattern (an iterable)."""
+    """Generator pattern. This example is both an iterable and an iterator."""
 
     def __init__(self, n):
         self.n = n
         self.num = 0
 
     def __iter__(self):
+        """An iterable has __iter__() which returns an iterator."""
         return self
 
     def next(self):
+        """An iterator has next() returning the next item in the iteration."""
         if self.num < self.n:
             current, self.num = self.num, self.num + 1
             return current
@@ -20,12 +22,21 @@ class FirstN(object):
 
 
 def firstN(n):
-    """Generator function."""
+    """Generator function.
+
+    Generator functions allow you to declare a function that behaves like an
+    iterator (i.e it can be used in a for loop).
+    """
     num = 0
     while num < n:
         yield num
         num += 1
 
 
+# Generator expression
+first_n_gen_exp = (x for x in range(1000000))
+
+
 assert sum(FirstN(1000000)) == 499999500000
 assert sum(firstN(1000000)) == 499999500000
+assert sum(first_n_gen_exp) == 499999500000
